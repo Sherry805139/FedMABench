ce4aa429affa8f496a85197d4e027af844f42dab
Sherry805139 <2066163267@qq.com>
2025-11-17 14:21:19 +0800

    fix OOM

--- bash/run_central_internvl2_2.sh
+++ bash/run_central_internvl2_2.sh
@@ -1,15 +1,18 @@
 # Use $2 if provided, otherwise use environment variable CUDA_VISIBLE_DEVICES
-CUDA_VISIBLE_DEVICES=${2:-${CUDA_VISIBLE_DEVICES:-0}} MAX_PIXELS=602112 \
+# Set memory optimization: expandable_segments to reduce fragmentation
+CUDA_VISIBLE_DEVICES=${2:-${CUDA_VISIBLE_DEVICES:-0}} \
+PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True \
+MAX_PIXELS=400000 \
   swift sft \
   --round 30 \
   --round_per_epoch 10 \
   --fed_alg central \
   --client_num 1 \
-  --model_type internvl2-8b \
-  --model_id_or_path /ailab/user/wangwenhao/.cache/modelscope/hub/OpenGVLab/InternVL2-8B \
+  --model_type qwen2-vl-2b-instruct \
+  --model_id_or_path /home/hmpiao/hmpiao/Qwen2-VL-2B-Instruct \
   --lazy_tokenize True \
   --preprocess_num_proc 4 \
-  --dataset $1 \
+  --dataset "/home/hmpiao/hmpiao/xuerong/FedMABench/android_control_unpack/episode-wise-conversations.jsonl" \
   --sft_type lora \
   --tuner_backend peft \
   --dtype AUTO \
@@ -17,7 +20,7 @@ CUDA_VISIBLE_DEVICES=${2:-${CUDA_VISIBLE_DEVICES:-0}} MAX_PIXELS=602112 \
   --train_dataset_sample -1 \
   --dataset_test_ratio 0 \
   --max_steps -1 \
-  --max_length 4096 \
+  --max_length 2048 \
   --check_dataset_strategy warning \
   --lora_rank 8 \
   --lora_alpha 32 \
@@ -26,7 +29,7 @@ CUDA_VISIBLE_DEVICES=${2:-${CUDA_VISIBLE_DEVICES:-0}} MAX_PIXELS=602112 \
   --batch_size 1 \
   --weight_decay 0.1 \
   --learning_rate 5e-5 \
-  --gradient_accumulation_steps 4 \
+  --gradient_accumulation_steps 8 \
   --max_grad_norm 0.5 \
   --warmup_ratio 0.03 \
   --eval_strategy no \
